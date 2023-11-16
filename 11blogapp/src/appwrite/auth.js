e9e4b2676a98797de89187b23600dcf434d36b88
hiteshchoudhary <hitesh@hiteshchoudhary.com>
2023-11-16 18:33:45 +0530

    add auth service for appwrite

--- 11blogapp/src/appwrite/auth.js
+++ 11blogapp/src/appwrite/auth.js
@@ -1,1 +1,56 @@
 // start with appwrite auth service
+import conf from "../conf/conf.js"
+import { Client, Account } from "appwrite";
+
+export class AuthService {
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId)
+        this.account = new Account(this.client)
+    }
+
+    async createAccount({email, password, name}){
+        try {
+            const userAccount = await this.account.create(ID.unique(), email, password, name)
+            if (userAccount) {
+                return this.login({email, password})
+            } else {
+                return userAccount
+            }
+        } catch (error) {
+            throw error
+        }
+    }
+    async login({email, password}){
+        try {
+            return await this.account.createEmailSession(email, password)
+        } catch (error) {
+            throw error
+        }
+    }
+    async getCurrentUser(){
+        try {
+            return await this.account.get()
+        } catch (error) {
+            console.log("Appwrite service :: getCurrentUser() :: ", error);
+        }
+        return null
+    }
+    async logout(){
+        try {
+            await this.account.deleteSessions()
+        } catch (error) {
+            console.log("Appwrite service :: logout() :: ", error);
+        }
+    }
+}
+
+
+
+const authService = new AuthService()
+
+export default authService
