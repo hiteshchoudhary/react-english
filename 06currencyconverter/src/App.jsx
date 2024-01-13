224621443c2ffb592d884e6a40850981a9a012a5
iGoodw1n <121541489+iGoodw1n@users.noreply.github.com>
2024-01-13 19:54:05 +0400

    Fix Swap functionality
    Bug with button without type

--- 06currencyconverter/src/App.jsx
+++ 06currencyconverter/src/App.jsx
@@ -47,6 +47,7 @@ function App() {
             </div>
             <div className='relative w-full h-0.5'>
               <button
+              type='button'
               className='absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5'
               onClick={swap}
               >Swap</button>
